21ec15e18e74163709406b082b86de4d4ee56102
AnkhSquirrel <suchkp@tutanota.com>
2022-11-08 15:37:39 +0100

    Clean start with empty index

---
+++ src/main/java/fr/kyo/crkf_web/ApplicationFacesConfig.java
@@ -0,0 +1,9 @@
+package fr.kyo.crkf_web;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig (version =  FacesConfig.Version.JSF_2_3)
+public class ApplicationFacesConfig {
+}
